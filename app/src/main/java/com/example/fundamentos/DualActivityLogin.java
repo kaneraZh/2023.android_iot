f8ddeefc9bb70f8f0da509549c7fac5d333b89aa
kaneraZh <ari1337ferrada@gmail.com>
2023-08-31 10:02:31 -0400

    principio trabajo en clases
    principio del trabajo en clases a terminar para el sabado

---
+++ app/src/main/java/com/example/fundamentos/DualActivityLogin.java
@@ -0,0 +1,58 @@
+package com.example.fundamentos;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.Nullable;
+public class DualActivityLogin extends AppCompatActivity{
+    private EditText user;
+    private EditText password;
+    private Button login;
+    public void setUser(EditText user) {
+        this.user = user;
+    }
+    private String get_user(){
+        try {
+            return this.user.getText().toString();
+        }
+        catch (Throwable t){
+            return "";
+        }
+    }
+    public void setPassword(EditText password) {
+        this.password = password;
+    }
+    private String get_password(){
+        try {
+            return this.password.getText().toString();
+        }
+        catch (Throwable t){
+            return "";
+        }
+    }
+    public void do_login(String usr, String pwd){
+        Intent profile = new Intent(this, DualActivityAccount.class);
+    }
+    public void setLogin(Button login) {
+        this.login = login;
+        this.login.setOnClickListener(
+                new View.OnClickListener(){
+                    @Override
+                    public void onClick(View view) {
+                        do_login(get_user(), get_password());
+                    }
+                }
+        );
+    }
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        setContentView(R.layout.activity_dual_login);
+        super.onCreate(savedInstanceState);
+        setLogin(findViewById(R.layout.));
+    }
+}
