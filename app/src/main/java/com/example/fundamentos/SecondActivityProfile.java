d4a05934d0fc350c5a2bdd8e2584b5cdad484c94
kaneraZh <ari1337ferrada@gmail.com>
2023-09-07 13:53:47 -0300

    lina3

--- app/src/main/java/com/example/fundamentos/SecondActivityProfile.java
+++ app/src/main/java/com/example/fundamentos/SecondActivityProfile.java
@@ -5,10 +5,64 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SecondActivityProfile extends AppCompatActivity {
+    private TextView titulo;
+    private TextView imagen;
+    private TextView alias;
+    private TextView cargo;
+    private TextView nombre;
+    private TextView edad;
+    private TextView telefono;
+    private TextView email;
+    private ImageView qr;
+
+    public void setTitulo(TextView titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setImagen(TextView imagen) {
+        this.imagen = imagen;
+    }
+
+    public void setAlias(TextView alias) {
+        this.alias = alias;
+    }
+
+    public void setCargo(TextView cargo) {
+        this.cargo = cargo;
+    }
+
+    public void setNombre(TextView nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEdad(TextView edad) {
+        this.edad = edad;
+    }
+
+    public void setTelefono(TextView telefono) {
+        this.telefono = telefono;
+    }
+
+    public void setEmail(TextView email) {
+        this.email = email;
+    }
+
+    public void setQr(ImageView qr) {
+        this.qr = qr;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second_profile);
+        setTitulo(findViewById(R.id.profile_titulo));
+        setImagen(findViewById(R.id.profile_imagen));
+        setAlias(findViewById(R.id.profile_alias));
+        setCargo(findViewById(R.id.profile_cargo));
+        setNombre(findViewById(R.id.profile_nombre));
+        setEdad(findViewById(R.id.profile_edad));
+        setTelefono(findViewById(R.id.profile_telefono));
+        setEmail(findViewById(R.id.profile_email));
+        setQr(findViewById(R.id.profile_qr));
     }
 }
--- app/src/main/java/com/example/fundamentos/SecondActivitySignin.java
+++ app/src/main/java/com/example/fundamentos/SecondActivitySignin.java
@@ -5,10 +5,57 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SecondActivitySignin extends AppCompatActivity {
+    private ImageView icono;
+    private EditText nombre;
+    private EditText nacimiento;
+    private EditText cargo;
+    private EditText email;
+    private EditText alias;
+    private EditText telefono;
+    private Button btn_login;
+
+    public void setIcono(ImageView icono) {
+        this.icono = icono;
+    }
+
+    public void setNombre(EditText nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setNacimiento(EditText nacimiento) {
+        this.nacimiento = nacimiento;
+    }
+
+    public void setCargo(EditText cargo) {
+        this.cargo = cargo;
+    }
+
+    public void setEmail(EditText email) {
+        this.email = email;
+    }
+
+    public void setAlias(EditText alias) {
+        this.alias = alias;
+    }
+
+    public void setTelefono(EditText telefono) {
+        this.telefono = telefono;
+    }
+
+    public void setBtn_login(Button btn_login) {
+        this.btn_login = btn_login;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second_signin);
+        setIcono(findViewById(R.id.profile_icono));
+        setNombre(findViewById(R.id.profile_nombre));
+        setNacimiento(findViewById(R.id.profile_nacimiento));
+        setCargo(findViewById(R.id.profile_cargo));
+        setEmail(findViewById(R.id.profile_email));
+        setAlias(findViewById(R.id.profile_alias));
+        setTelefono(findViewById(R.id.profile_telefono));
+        setBtn_login(findViewById(R.id.btn_login));
     }
 }
