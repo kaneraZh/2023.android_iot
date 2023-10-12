58977556f622a83c83e23d03b4753676c7ac1603
kaneraZh <ari1337ferrada@gmail.com>
2023-10-12 12:01:34 -0300

    fragment
    tabs layout basic

--- app/src/main/java/com/example/fundamentos/Fragment0.java
+++ app/src/main/java/com/example/fundamentos/Fragment0.java
@@ -17,6 +17,7 @@ public class Fragment0 extends Fragment {
     public Fragment0(){
         super(R.layout.fragment_0);
     }
+
 //    // TODO: Rename parameter arguments, choose names that match
 //    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 //    private static final String ARG_PARAM1 = "param1";
