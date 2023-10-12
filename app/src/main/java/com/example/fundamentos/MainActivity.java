50bd252a37e6908ce818d1de1b9019fcd9810b3e
kaneraZh <ari1337ferrada@gmail.com>
2023-10-12 10:44:42 -0300

    firebase setup
    firebase setup, pag 1 de parte 3 crud con tabs

--- app/src/main/java/com/example/fundamentos/MainActivity.java
+++ app/src/main/java/com/example/fundamentos/MainActivity.java
@@ -1,8 +1,6 @@
 package com.example.fundamentos;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
     @Override
