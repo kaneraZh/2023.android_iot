0dbb47b74407aea4ec7b579a6e56cc042290f615
kaneraZh <ari1337ferrada@gmail.com>
2023-10-12 11:11:08 -0300

    +1
    plus one

--- app/src/main/java/com/example/fundamentos/MainActivity.java
+++ app/src/main/java/com/example/fundamentos/MainActivity.java
@@ -1,13 +1,44 @@
 package com.example.fundamentos;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 public class MainActivity extends AppCompatActivity {
+    private int count = 0;
+    public void addCount(){count++;}
+    private TextView display_count;
+    public TextView getDisplayCount() {return display_count;}
+    public void setDisplayCount(TextView display_count) {
+        this.display_count = display_count;
+        updateDisplay();
+    }
+    public void updateDisplay() {
+        this.display_count.setText( Integer.toString(this.count) );
+    }
+
+    private Button adder;
+    public Button getAdder() {return adder;}
+    public void setAdder(Button adder) {
+        this.adder = adder;
+        this.adder.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        addCount();
+                        updateDisplay();
+                    }
+                }
+        );
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setDisplayCount(findViewById(R.id.display_count));
+        setAdder(findViewById(R.id.adder));
+        this.count = 0;
     }
 }
