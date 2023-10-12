3c51691420ac254f52e65c4802c3fad905f914a5
kaneraZh <ari1337ferrada@gmail.com>
2023-10-12 11:38:08 -0300

    string
    suma pero con string, pesima practica

--- app/src/main/java/com/example/fundamentos/MainActivity.java
+++ app/src/main/java/com/example/fundamentos/MainActivity.java
@@ -6,16 +6,18 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private int count = 0;
-    public void addCount(){count++;}
+//    private int count = 0;
+//    public void addCount(){count++;}
     private TextView display_count;
     public TextView getDisplayCount() {return display_count;}
     public void setDisplayCount(TextView display_count) {
         this.display_count = display_count;
-        updateDisplay();
+        this.display_count.setText( Integer.toString(0) );
     }
     public void updateDisplay() {
-        this.display_count.setText( Integer.toString(this.count) );
+        int count = Integer.parseInt(this.display_count.getText().toString());
+        this.display_count.setText( Integer.toString(count+1) );
+//        this.display_count.setText( Integer.toString(this.count) );
     }
 
     private Button adder;
@@ -26,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        addCount();
+//                        addCount();
                         updateDisplay();
                     }
                 }
@@ -39,6 +41,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setDisplayCount(findViewById(R.id.display_count));
         setAdder(findViewById(R.id.adder));
-        this.count = 0;
+//        this.count = 0;
     }
 }
