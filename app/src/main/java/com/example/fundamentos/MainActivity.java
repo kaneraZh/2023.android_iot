4de9bffa1f7799f8ebf62bf2365de1c3c1cdede1
kaneraZh <ari1337ferrada@gmail.com>
2023-10-19 09:43:48 -0300

    main login
    main login xml hecho

--- app/src/main/java/com/example/fundamentos/MainActivity.java
+++ app/src/main/java/com/example/fundamentos/MainActivity.java
@@ -1,9 +1,8 @@
 package com.example.fundamentos;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
+
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
